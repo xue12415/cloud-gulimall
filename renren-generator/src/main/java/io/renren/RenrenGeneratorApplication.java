5f6ebe1da2f7e706842e1bfb4466cbc4d8e08bf1
xue <810911851@qq.com>
2020-11-13 17:51:06 +0800

    TEST

--- renren-generator/src/main/java/io/renren/RenrenGeneratorApplication.java
+++ renren-generator/src/main/java/io/renren/RenrenGeneratorApplication.java
@@ -1,13 +1,16 @@
-package com.example;
+package io.renren;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@MapperScan("io.renren.dao")
 public class RenrenGeneratorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RenrenGeneratorApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(RenrenGeneratorApplication.class, args);
+	}
 }
