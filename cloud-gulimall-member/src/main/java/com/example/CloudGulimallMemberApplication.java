2d1a40e36399d60a5e4e3d33639ee46a9406b195
xue <810911851@qq.com>
2020-11-13 15:50:36 +0800

    TEST

---
+++ cloud-gulimall-member/src/main/java/com/example/CloudGulimallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGulimallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGulimallMemberApplication.class, args);
+    }
+
+}
