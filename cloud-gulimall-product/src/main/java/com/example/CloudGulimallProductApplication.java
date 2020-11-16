f7691863707815160023f6808e4827d4203f1c52
xue <810911851@qq.com>
2020-11-16 17:58:05 +0800

    TEST

--- cloud-gulimall-product/src/main/java/com/example/CloudGulimallProductApplication.java
+++ cloud-gulimall-product/src/main/java/com/example/CloudGulimallProductApplication.java
@@ -3,10 +3,12 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @MapperScan("com.example.modules.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudGulimallProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudGulimallProductApplication.class, args);
